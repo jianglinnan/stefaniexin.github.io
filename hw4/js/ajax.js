a1e1599b915d6f8965391b953a5883709667ed47
stefaniexin <stefanie_xin@163.com>
2014-07-10 22:18:42 +0800

    hw4

---
+++ hw4/js/ajax.js
@@ -0,0 +1,49 @@
+var xmlHttp;
+
+function processData(data){
+    var newsList = $('a');
+    for(var i in newsList){
+        $(newsList[i]).attr('title',data.newsInfo[i]['title']);
+        $(newsList[i]).attr('rel',data.newsInfo[i]['pictureLink']);
+    }
+}
+
+function createXMLHttpRequest(){
+    if(window.ActiveXObject)
+    {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else if(window.XMLHttpRequest)
+    {
+        xmlHttp = new XMLHttpRequest();
+    }
+}
+function startRequest(){
+    createXMLHttpRequest();
+    try
+    {
+        xmlHttp.onreadystatechange = handleStateChange;
+        xmlHttp.open("GET", "news.json", false);
+        xmlHttp.send(null);
+    }
+    catch(exception)
+    {
+        alert("xmlHttp Fail");
+    }
+}
+function handleStateChange(){   
+    if(xmlHttp.readyState == 4)
+    {       
+        if (xmlHttp.status == 200)
+        {
+            var result = xmlHttp.responseText;
+            json = eval("(" + result + ")");
+            processData(json);
+        }
+    }
+}
+
+
+$(document).ready(function(){
+    startRequest();
+});
