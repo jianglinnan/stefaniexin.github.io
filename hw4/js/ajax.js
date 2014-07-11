c3c6ec399ea405908a4ccb1fe7dac334cde5eebe
stefaniexin <stefanie_xin@163.com>
2014-07-11 16:08:58 +0800

    hw4

--- hw4/js/ajax.js
+++ hw4/js/ajax.js
@@ -3,7 +3,7 @@ var xmlHttp;
 function processData(data){
     var newsList = $('a');
     for(var i in newsList){
-        $(newsList[i]).attr('title',data.newsInfo[i]['title']);
+        //$(newsList[i]).attr('title',data.newsInfo[i]['title']);
         $(newsList[i]).attr('rel',data.newsInfo[i]['pictureLink']);
         $(newsList[i]).attr('href',data.newsInfo[i]['newsLink']);
     }
